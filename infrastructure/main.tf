9054d0104ce7441dcc87c925e2418bfbf4ae1cec
varapalla <vpalla@tillster.com>
2023-04-24 17:39:12 +0530

    Initial code checkin

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -59,5 +59,17 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     ssl_support_method = "sni-only"
   }
 
+  restriction {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US"]
+    }
+
+    ip_restriction {
+      # Replace with your own list of IP addresses
+      ip_address = ["192.0.2.0/24"]
+    }
+  }
+
   #aliases = ["my-angular-frontend.example.com"]
 }
