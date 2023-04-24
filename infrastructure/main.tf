22b49e25631cc13704be726923c06ae952c2b28e
varapalla <vpalla@tillster.com>
2023-04-24 16:38:43 +0530

    Initial code checkin

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "<YOUR_ACCESS_KEY>"
-  secret_key = "<YOUR_SECRET_KEY>"
-  region     = "<YOUR_AWS_REGION>"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.aws_region
   default_tags {
     tags = {
         Terraform   = "true"
