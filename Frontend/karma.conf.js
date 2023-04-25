8437320210331a97d23a985ac031fddf2a3fd5dc
varapalla <vpalla@tillster.com>
2023-04-25 12:24:29 +0530

    Replaced karma js file

--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -1,44 +1,65 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/food-ordering-app'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
-};
+  // process.env.CHROME_BIN = require('puppeteer').executablePath();
+   // config.environment.
+   config.set({
+     basePath: '',
+     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+     plugins: [
+       require('karma-jasmine'),
+       require('karma-chrome-launcher'),
+       require('karma-jasmine-html-reporter'),
+       require('karma-coverage'),
+       require('@angular-devkit/build-angular/plugins/karma'),
+     ],
+     client: {
+       captureConsole: false,
+       clearContext: false, // leave Jasmine Spec Runner output visible in browser
+       jasmine: {
+         random: true
+       }
+     },
+     coverageReporter: {
+       dir: require('path').join(__dirname, 'coverage'),
+       reporters: [
+         // reporters not supporting the `file` property
+         { type: 'html'},
+         // reporters supporting the `file` property, use `subdir` to directly
+         // output them in the `dir` directory
+         { type: 'lcovonly' },
+         { type: 'text-summary'},
+         { type: 'cobertura', file: 'test-results.xml'},
+         { type: 'json', file: 'coverage-final.json'}
+       ],
+       subdir: '.',
+       check: {
+         global: {
+           statements: 70,
+           branches: 55,
+           lines: 70,
+           functions: 70,
+         }
+       }
+     },
+     reporters: ['progress','kjhtml'],
+     port: 9876,
+     colors: true,
+     logLevel: config.LOG_INFO,
+     autoWatch: true,
+     browserDisconnectTimeout: 10000,
+     browserDisconnectTolerance: 3,
+     browserNoActivityTimeout: 60000,
+     browsers: ['ChromeHeadlessNoSandbox'],
+
+     customLaunchers: {
+       ChromeHeadlessNoSandbox: {
+         base: 'ChromeHeadless',
+         flags: ['--no-sandbox']
+       }
+     },
+     singleRun: false,
+     browserNoActivityTimeout: 2000000
+   });
+ };
